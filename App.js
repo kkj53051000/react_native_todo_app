670ff8d69ced754cc7cfa360362529f339d70e4e
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-22 10:57:10 +0900

    setting DateHead

--- App.js
+++ App.js
@@ -8,19 +8,23 @@
 import React from 'react';
 import {
   SafeAreaView,
-  Text,
-  View,
+  StyleSheet
 } from 'react-native';
+import DateHead from './components/DateHead';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
+  const today = new Date();
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hi ReactNative</Text>
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <DateHead date={today} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default App;
