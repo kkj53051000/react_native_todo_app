834644275abb3f04b22c0643275de6f0a3386eb1
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-22 11:11:49 +0900

    ready layout

--- App.js
+++ App.js
@@ -12,19 +12,27 @@ import {
 } from 'react-native';
 import DateHead from './components/DateHead';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 const App = () => {
   const today = new Date();
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
