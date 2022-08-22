dc207ae461f08176e918e95fbb51dc713b3e83d0
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-22 11:43:29 +0900

    add customButton

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
-import {View, StyleSheet, TextInput} from 'react-native';
+import 
+    {View,
+    StyleSheet,
+    TextInput,
+    Image,
+    TouchableOpacity,
+    Platform,
+    TouchableNativeFeedback}
+from 'react-native';
 
 function AddTodo() {
 
     const [text, setText] = useState('');
+
+    const onPress = () => {
+        setText('');
+        Keyboard.dismiss();
+    };
 
     return (
         <View style={styles.block} >
@@ -12,23 +25,54 @@ function AddTodo() {
                 style={styles.input}
                 value={text}
                 onChange={setText}
+                returnKeyType="done"
             />
+            {Platform.OS === 'ios' ? (
+                <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+                    <View style={styles.buttonStyle}>
+                        <Image source={require('../assets/icons/add_white/add_white.png')} />
+                    </View>
+                </TouchableOpacity>
+            ) : (
+                <View style={styles.circleWrapper} onPress={onPress}>
+                    <TouchableNativeFeedback>
+                        <View style={styles.buttonStyle}>
+                            <Image source={require('../assets/icons/add_white/add_white.png')} />
+                        </View>
+                    </TouchableNativeFeedback>
+                </View>
+            )}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     block: {
+        backgroundColor: 'white',
         height: 64,
         paddingHorizontal: 16,
         borderColor: '#bdbdbd',
         borderTopWidth: 1,
         borderBottomWidth: 1,
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     input: {
+        flex: 1,
         fontSize: 16,
         paddingVertical: 8,
+    },
+    buttonStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 48,
+        height: 48,
+        backgroundColor: '#26a69a',
+        borderRadius: 24,
+    },
+    circleWrapper: {
+        overflow: 'hidden',
+        borderRadius: 24,
     },
 });
 
