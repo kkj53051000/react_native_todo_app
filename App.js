1e8743466919615936f48a2fe8d180fe545aa0f1
kkj53051000 <tkarnrwl7418@naver.com>
2022-08-22 10:32:08 +0900

    start TodoApp

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hi ReactNative</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
